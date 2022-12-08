27ce748710c6371bd80c457b826132f6b811ba99
Cavid <hcavid386@gmail.com>
2022-12-08 09:43:49 -0800

    Add project files.

---
+++ JsonListDemo/JsonList.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonListDemo
+{
+    public class JsonList
+    {
+        private static string _fileName = "Name.json";
+        private List<string> list;
+
+        public void Add(string item)
+        {
+            list = GetList();
+            list.Add(item);
+            WriteList(list);
+        }
+        public bool Search(string item)
+        {
+            list = GetList();
+            return list.Contains(item);
+        }
+        public void Update(int index, string item)
+        {
+            list = GetList();
+            list[index] = item;
+            WriteList(list);
+        }
+       public void  Delete(int index) { }
+
+
+        private List<string> GetList()
+        {
+            List<string>? deserializedData;
+            try
+            {
+                string fileData = File.ReadAllText(_fileName);
+                deserializedData = JsonConvert.DeserializeObject<List<string>>(fileData);
+                return deserializedData;
+            }
+            catch (Exception)
+            {
+                deserializedData = new List<string>();
+                return deserializedData;
+            }
+
+        }
+        private void WriteList(List<string> data)
+        {
+            string serializedList = JsonConvert.SerializeObject(data);
+            try
+            {
+                File.WriteAllText(_fileName, serializedList);
+
+            }
+            catch (Exception)
+            {
+                File.Create(_fileName);
+                WriteList(data);
+            }
+        }
+    }
+}
---
+++ JsonListDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿//using Newtonsoft.Json;
+
+//List<string> adlar = new List<string> { "salam", "Sadsad", "asdasd" };
+
+
+//var t = JsonConvert.SerializeObject(adlar);
+//Console.WriteLine(t);
+//File.WriteAllText("data.json", t);
+
+
+//var tt=File.ReadAllText("data.json");
+//var cc = JsonConvert.DeserializeObject<List<string>>(tt);
+//int k = 4;
+
+
+
+using JsonListDemo;
+
+JsonList t=new JsonList();
+
+t.Add("4324234234");
+t.Add("4324234234");
+//t.Add("cavid");
+t.Add("4324234234");
+t.Add("4324234234");
+t.Update(0, "Fuck of");
+
+
+
+
+Console.WriteLine(t.Search("cavid"));
